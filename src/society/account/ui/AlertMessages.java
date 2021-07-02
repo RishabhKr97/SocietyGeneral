36a26275ea9fbaa347d07cf7146f87722321a788
penguin2048 <rishabhkr1729@gmail.com>
2021-07-02 18:33:42 +0530

    Add Alert Box Manager Class

---
+++ src/society/account/ui/AlertMessages.java
@@ -0,0 +1,21 @@
+package society.account.ui;
+
+import java.awt.Component;
+
+import javax.swing.JOptionPane;
+
+public class AlertMessages {
+
+	public static void showErrorMessage(Component component, String message) {
+		JOptionPane.showMessageDialog(component, message, "Input Error", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void showAlertMessage(Component component, String message) {
+		JOptionPane.showMessageDialog(component, message, "Alert", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void showSystemErrorMessage(Component component) {
+		JOptionPane.showMessageDialog(component, "System Error Occured. Contact Administrator.", "Error",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+}
