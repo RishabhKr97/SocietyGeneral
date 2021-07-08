5287403e70da26cceef04a1541b74a4da38d9ead
penguin2048 <rishabhkr1729@gmail.com>
2021-07-08 21:45:40 +0530

    Add functionality to print previous transaction

--- src/society/account/ui/TransactionPanel.java
+++ src/society/account/ui/TransactionPanel.java
@@ -7,6 +7,7 @@ import javax.swing.JTabbedPane;
 
 import society.account.ui.subPanels.AddTransactionPanel;
 import society.account.ui.subPanels.EditTransactionPanel;
+import society.account.ui.subPanels.GenerateReceiptPanel;
 import society.account.ui.subPanels.SearchTransactionPanel;
 
 @SuppressWarnings("serial")
@@ -15,6 +16,7 @@ public class TransactionPanel extends JPanel {
 	JTabbedPane mTabs;
 	AddTransactionPanel mAddTransactionPanel;
 	SearchTransactionPanel mSearchTransactionPanel;
+	GenerateReceiptPanel mGenerateReceiptPanel;
 	EditTransactionPanel mEditTransactionPanel;
 
 	TransactionPanel() {
@@ -22,12 +24,14 @@ public class TransactionPanel extends JPanel {
 		mTabs = new JTabbedPane();
 		mAddTransactionPanel = new AddTransactionPanel();
 		mSearchTransactionPanel = new SearchTransactionPanel();
+		mGenerateReceiptPanel = new GenerateReceiptPanel();
 		mEditTransactionPanel = new EditTransactionPanel();
 
 		mTabs.setSize(UiConstants.DimensionConstants.WINDOW_DIMENSION, UiConstants.DimensionConstants.WINDOW_DIMENSION);
 		mTabs.setFont(UiFontManager.getSubHeadingFont());
 		mTabs.add("Add New Transaction", mAddTransactionPanel);
 		mTabs.add("Search Transaction", mSearchTransactionPanel);
+		mTabs.add("Generate Transaction Receipt", mGenerateReceiptPanel);
 		mTabs.add("Edit Transaction", mEditTransactionPanel);
 		add(mTabs);
 	}
---
+++ src/society/account/ui/subPanels/GenerateReceiptPanel.java
@@ -0,0 +1,75 @@
+package society.account.ui.subPanels;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import society.account.database.DatabaseHelper;
+import society.account.receipt.printmanager.Printer;
+import society.account.ui.AlertMessages;
+import society.account.ui.UiConstants;
+
+@SuppressWarnings("serial")
+public class GenerateReceiptPanel extends JPanel implements ActionListener  {
+
+	private int mWidth = UiConstants.DimensionConstants.DEFAULT_WIDTH;
+	private int mHeight = UiConstants.DimensionConstants.DEFAULT_HEIGHT;
+	private int mInitialSpacing = UiConstants.DimensionConstants.DEFAULT_INITIAL_SPACING;
+	private int mHorizontalSpacing = UiConstants.DimensionConstants.DEFAULT_HORIZONTAL_SPACING;
+	
+	private JLabel mTransactionNumberLabel;
+	private JTextField mTransactionNumberValue;
+	private JButton mSubmit;
+
+	private final DatabaseHelper dbHelper = new DatabaseHelper();
+	
+	public GenerateReceiptPanel() {
+		setLayout(null);
+		
+		mTransactionNumberLabel = new JLabel("Enter Transaction Number");
+		mTransactionNumberLabel.setSize(mWidth, mHeight);
+		mTransactionNumberLabel.setLocation(mInitialSpacing, mInitialSpacing);
+		add(mTransactionNumberLabel);
+
+		mTransactionNumberValue = new JTextField();
+		mTransactionNumberValue.setSize(mWidth, mHeight);
+		mTransactionNumberValue.setLocation(mHorizontalSpacing, mInitialSpacing);
+		add(mTransactionNumberValue);
+
+		mSubmit = new JButton("Generate Receipt");
+		mSubmit.setSize(200, mHeight);
+		mSubmit.setLocation(mHorizontalSpacing * 2, mInitialSpacing);
+		mSubmit.addActionListener(this);
+		add(mSubmit);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == mSubmit) {
+			String transactionNum = mTransactionNumberValue.getText().trim();
+			
+			if (!dbHelper.checkTransactionNumberValid(transactionNum)) {
+				AlertMessages.showAlertMessage(this, "Transaction Number Does Not Exist");
+				mTransactionNumberValue.setText("");
+				return;
+			}
+			
+			Map<String, String> values = dbHelper.getTransactionInfo(transactionNum);
+			if (values == null || !Printer.printTransaction(values.get("account_number"), transactionNum, this)) {
+				AlertMessages.showSystemErrorMessage(this);
+				mTransactionNumberValue.setText("");
+				return;				
+			}
+			
+			mTransactionNumberValue.setText("");
+			AlertMessages.showAlertMessage(this, "Receipt Generated!");			
+		}		
+	}
+
+}
